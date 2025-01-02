06cafa7899ba3e927d5a9a701abec7baa5ea63ad
eungbin <programmer97@naver.com>
2025-01-03 04:29:28 +0900

    Update Header's style

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,8 +3,8 @@ import {  } from 'react';
 export default function Header() {
 
   return (
-    <header className='w-full h-16 bg-white flex flex-row flex-wrap content-center justify-between px-12'>
-      <span className='cursor-pointer text-4xl'>LOA</span>
+    <header className='w-full h-16 bg-white flex flex-row flex-wrap content-center justify-between px-12 shadow-md shadow-indigo-100'>
+      <span className='cursor-pointer text-4xl text-indigo-500'>LOA</span>
       <section className='flex content-center flex-wrap gap-x-8'>
         <p className='cursor-pointer text-slate-500 font-bold font-sans text-base hover:text-indigo-400'>공지사항</p>
         <p className='cursor-pointer text-slate-500 font-bold font-sans text-base hover:text-indigo-400'>캐릭터 검색</p>
