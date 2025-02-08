b0436f46e47461c8a3155f42aacd10dc6bbd9bc0
eungbin <programmer97@naver.com>
2025-02-08 22:31:35 +0900

    Update table

--- src/pages/Synergy/Synergy.tsx
+++ src/pages/Synergy/Synergy.tsx
@@ -35,17 +35,17 @@ export default function Synergy() {
   }, []);
 
   return (
-    <section className='w-full h-full flex justify-center'>
-      <table className='table-auto'>
+    <section className='w-full h-full flex justify-center mt-8'>
+      <table className='table-auto bg-white rounded-xl shadow-xl'>
         <thead>
-          <tr>
-            <th>시너지</th>
-            <th>직업</th>
+          <tr className='border-b-2 border-indigo-500 text-indigo-700 border-dotted'>
+            <th className='p-4'>시너지</th>
+            <th className=''>직업</th>
           </tr>
         </thead>
         <tbody>
           {synergy.map((v,idx) => (
-            <tr key={idx}><td>{v.synergy}</td><td>{v.class.join(', ')}</td></tr>
+            <tr key={idx}><td className='p-4 px-8 text-sm font-bold text-indigo-500'>{v.synergy}</td><td className='p-4 px-8 text-sm'>{v.class.join(', ')}</td></tr>
           ))}
         </tbody>
       </table>
