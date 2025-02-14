b385ecc589d802be68028b5df778c3d40cba4225
eungbin <programmer97@naver.com>
2025-02-15 00:56:20 +0900

    Add many func 1. 공대원 추가 시 겹치는 시너지는 다른 파티로 들어가도록 조정 2. 시너지가 모두 겹칠 경우 들어가지 않도록 조정

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -1,17 +1,29 @@
 import React, { useState } from 'react';
 
-export default function Box() {
+interface IParty {
+  class: string[];
+  synergy: string[];
+}
+
+interface IBox {
+  index: number;
+  removeBox: (index: number) => void;
+  members: IParty;
+}
+
+export default function Box(props: IBox) {
+  const _removeBox = () => { props.removeBox(props.index); }
 
   return (
     <div className='rounded-xl px-8 py-4 bg-white shadow-md relative w-72'>
-      <div className='rounded-full border flex justify-center items-center border-white-500 bg-white text-xs h-6 absolute -top-2 -right-1 w-6 cursor-pointer active:bg-white/50'>
+      <div onClick={_removeBox} className='rounded-full border flex justify-center items-center border-white-500 bg-white text-xs h-6 absolute -top-2 -right-1 w-6 cursor-pointer active:bg-white/50'>
         <p className='text-red-500'>X</p>
       </div>
       <div className='flex flex-row justify-around flex-wrap gap-y-2 text-sm'>
-        <input className='border border-slate-500 w-24 flex justify-center items-center rounded-md p-1 text-center' />
-        <input className='border border-slate-500 w-24 flex justify-center items-center rounded-md p-1 text-center' />
-        <input className='border border-slate-500 w-24 flex justify-center items-center rounded-md p-1 text-center' />
-        <input className='border border-slate-500 w-24 flex justify-center items-center rounded-md p-1 text-center' defaultValue={'서포터'} disabled />
+        <input className='border outline-indigo-500 w-24 flex justify-center items-center rounded-md p-1 text-center' defaultValue={props.members?.class[0]} disabled />
+        <input className='border outline-indigo-500 w-24 flex justify-center items-center rounded-md p-1 text-center' defaultValue={props.members?.class[1]} disabled />
+        <input className='border outline-indigo-500 w-24 flex justify-center items-center rounded-md p-1 text-center' defaultValue={props.members?.class[2]} disabled />
+        <input className='border outline-indigo-500 w-24 flex justify-center items-center rounded-md p-1 text-center' defaultValue={'서포터'} disabled />
       </div>
     </div>
   );
--- src/pages/Synergy/Synergy.tsx
+++ src/pages/Synergy/Synergy.tsx
@@ -1,14 +1,17 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import synergyData from '../../assets/Synergy.json';
 import Box from '../../components/Box/Box';
 
+const classList = synergyData.data.map((item: { class: string }) => item.class) as readonly string[];
+type ClassType = (typeof classList)[number];
+
 interface ISynergy {
   synergy: string;
-  class: Array<string>;
+  class: Array<ClassType>;
 }
-
 interface IParty {
-  class: string;
+  class: ClassType[];
+  synergy: string[];
 }
 
 export default function Synergy() {
@@ -27,7 +30,8 @@ export default function Synergy() {
     { synergy: '마나 회복', class: [] }
   ]);
 
-  const [partys, setPartys] = useState<IParty[][]>([]);
+  const [parties, setParties] = useState<IParty[]>([]);
+  const memberRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     const gettedSynergy: ISynergy[] = [...synergy];
@@ -41,16 +45,76 @@ export default function Synergy() {
     }
   }, []);
 
-  const _addParty = () => {
-    setPartys([
-      ...partys,
-      []
-    ]);
+  /**
+   * 입력된 클래스가 유효한지 아닌지 검삭해주는 함수
+   * @param value 입력된 클래스
+   * @returns (boolean) 유효한 직업인지 아닌지
+   */
+  const _isValidClass = (value: string): value is ClassType => {
+    return classList.includes(value as ClassType);
+  }
+
+  /**
+   * 해당 직업의 시너지 목록을 반환해주는 함수
+   * @param value 
+   * @returns string[]
+   */
+  const _getSynergy = (value: ClassType): string[] => {
+    return synergy.filter((v) => v.class.includes(value))
+                  .map((v) => v.synergy);
+  }
+
+  /**
+   * 파티를 늘려주는 함수
+   */
+  const _addParty = () => { setParties([...parties, { class:[],synergy:[] }]); }
+
+  /**
+   * index에 맞는 파티를 제거해주는 함수
+   * @param index 
+   */
+  const _removeParty = (index: number) => {
+    const copiedParties = [...parties];
+    setParties([...copiedParties.filter((v,idx) => idx !== index)]);
+  }
+
+  /**
+   * 빈 파티에 멤버를 추가해주는 함수
+   * @returns 
+   */
+  const _addMember = () => {
+    let flag: boolean = false;
+
+    if(memberRef.current?.value === '') { alert('클래스를 입력해 주세요.'); return; }
+    if(parties.length === 0) { alert('파티가 존재하지 않습니다.'); return; }
+    if(!(_isValidClass(memberRef.current?.value))) { alert('정확한 클래스명을 입력해 주세요.'); return; }
+
+    const copiedParties = [...parties];
+    for(let i=0; i<copiedParties.length; i++) {
+      if(copiedParties[i].class.length === 3) continue;
+      else {
+        const typedClass = memberRef.current?.value.toString();
+        const addedSynergies: string[] = _getSynergy(typedClass).filter(v => !copiedParties[i].synergy.includes(v)).map(v => {
+          copiedParties[i].synergy.push(v);
+          return v;
+        });
+        
+        if(addedSynergies.length === 0) { flag = true; continue; }
+
+        copiedParties[i].class.push(typedClass);
+        setParties([...copiedParties]);
+        memberRef.current.value = '';
+        return; 
+      }
+    }
+
+    if(flag) { alert('해당 직업이 들어갈 적정한 파티가 존재하지 않습니다.'); return; }
+    if(memberRef.current?.value !== '') { alert('빈자리가 존재하지 않습니다.'); return; }
   }
 
   return (
     <section className='w-full h-full flex justify-center mt-8'>
-      <section className='flex flex-row gap-x-8 min-w-120'>
+      <section className='flex flex-row gap-x-8'>
         <table className='table-auto bg-white rounded-xl shadow-xl'>
           <thead className=''>
             <tr className='border-b-2 border-indigo-500 text-indigo-700 border-dotted'>
@@ -64,13 +128,19 @@ export default function Synergy() {
             ))}
           </tbody>
         </table>
-        <section className='flex flex-col gap-y-4 w-xl'>
-          <button className='py-2 px-4 rounded-xl shadow-md active:shadow-none w-28' onClick={_addParty}>
-            <p className='text-slate-500'>파티 추가</p>
+        <section className='flex flex-col gap-y-4' style={{width:''}}>
+          <button className='py-2 px-4 rounded-md shadow-md active:shadow-none active:bg-indigo-400 w-28 bg-indigo-500' onClick={_addParty}>
+            <p className='text-white'>파티 추가</p>
           </button>
+          <div className='flex flex-row gap-x-4'>
+            <input className='border w-28 flex rounded-md py-1 text-center focus:outline-indigo-500 shadow-md' placeholder='클래스 입력' ref={memberRef} />
+            <button className='py-2 px-4 rounded-md shadow-md active:shadow-none active:bg-indigo-400 w-28 bg-indigo-500' onClick={_addMember}>
+              <p className='text-white'>공대원 추가</p>
+            </button>
+          </div>
           <section className='flex gap-x-4'>
-              {partys.map(v => (
-                <Box />
+              {parties.map((v, idx) => (
+                <Box key={idx} index={idx} removeBox={_removeParty} members={v} />
               ))}
           </section>
         </section>
